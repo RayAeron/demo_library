1b35f0849cf9df8b27366600082eb6f546d0fe0b
Ray Aeron <rayaeron95@hotmail.com>
2021-05-20 11:16:06 +0300

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace demo_library
+{
+    public class Class1
+    {
+        //string s1 = "hello";
+        //string s2 = null;
+
+        //string s3 = new String('a', 6); // результатом будет строка "aaaaaa"
+        //string s4 = new String(new char[] { 'w', 'o', 'r', 'l', 'd' });
+    }
+}
